a2079b16104fe9ea58e451f9fcbeff287986206a
Pii <pii@Piis-iMac.local>
2020-02-26 20:55:40 -0300

    Organização do projeto

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {SafeAreaView, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <SafeAreaView>
+      <Text>Olá login</Text>
+    </SafeAreaView>
+  );
+};
+
+export default App;
