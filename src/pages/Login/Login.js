e5e6c4ef4dbc2d89a807ce8baa9bb5e9b4f953a1
Pii <pii@Piis-iMac.local>
2020-02-28 14:16:30 -0300

    Instalação dos icones

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {SafeAreaView, Text} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const Login = () => {
   return (
     <SafeAreaView>
-      <Text>Olá login</Text>
+      <Icon name="home" />
     </SafeAreaView>
   );
 };
